2006a19fcdf380562adc9fcae5679b0d13d9165e
Emil Madsen <emilmadsen1993@gmail.com>
2022-03-16 22:51:16 +0100

    context test - mock discordclient

--- src/test/java/dk/emilmadsen/deluminator/AppTests.java
+++ src/test/java/dk/emilmadsen/deluminator/AppTests.java
@@ -1,10 +1,15 @@
 package dk.emilmadsen.deluminator;
 
+import discord4j.core.DiscordClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class AppTests {
+
+	@MockBean
+	private DiscordClient discordClient;
 
 	@Test
 	void contextLoads() {
